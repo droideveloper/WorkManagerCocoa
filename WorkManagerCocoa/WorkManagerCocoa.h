279b0b86f373586e77e297d33a1d308b6ec26ede
droideveloper <fatih.sen.boun@gmail.com>
2020-01-18 20:12:40 +0300

    Initial Commit

---
+++ WorkManagerCocoa/WorkManagerCocoa.h
@@ -0,0 +1,19 @@
+//
+//  WorkManagerCocoa.h
+//  WorkManagerCocoa
+//
+//  Created by Fatih Sen on 18.01.2020.
+//  Copyright © 2020 Fatih Sen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WorkManagerCocoa.
+FOUNDATION_EXPORT double WorkManagerCocoaVersionNumber;
+
+//! Project version string for WorkManagerCocoa.
+FOUNDATION_EXPORT const unsigned char WorkManagerCocoaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WorkManagerCocoa/PublicHeader.h>
+
+
